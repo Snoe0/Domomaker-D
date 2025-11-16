e3874c37ae5166e739655269dae12b8d01701a79
Snoe0 <yurikorolev99@gmail.com>
2025-11-16 00:36:28 -0500

    fix | signup func

--- server/router.js
+++ server/router.js
@@ -5,6 +5,8 @@ const router = (app) => {
   app.get('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.loginPage);
   app.post('/login', mid.requiresSecure, mid.requiresLogout, controllers.Account.login);
 
+  app.post('/signup', mid.requiresSecure, mid.requiresLogout, controllers.Account.signup);
+
   app.get('/logout', mid.requiresLogin, controllers.Account.logout);
 
   app.get('/maker', mid.requiresLogin, controllers.Domo.makerPage);
